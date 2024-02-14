d7ebda0aab20caf15ddd5a6b1dde460a0566c901
Asphalt52 <113107740+ChrisCross52@users.noreply.github.com>
2024-02-14 13:54:12 +0100

    Update DHT22Dock.py

--- DHT22Dock.py
+++ DHT22Dock.py
@@ -8,3 +8,19 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+"""
+from machine import Pin
+from time import sleep
+import dht
+
+capteur = dht.DHT22(Pin(23))
+
+while True:
+  try:
+    sleep(1)     # le DHT22 renvoie au maximum une mesure toute les 2s
+    capteur.measure()     # Recuperère les mesures du capteur
+    print(f"Temperature : {capteur.temperature():.1f}°C")
+    print(f"Humidite    : {capteur.humidity():.1f}%")
+  except OSError as e:
+    print("Echec reception")
+      """
