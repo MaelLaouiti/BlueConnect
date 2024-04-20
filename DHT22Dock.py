9d138419689e0322f4bf128a67a321357c997b41
Asphalt52 <113107740+ChrisCross52@users.noreply.github.com>
2024-04-20 15:14:24 +0200

    Update DHT22.py

--- DHT22Dock.py
+++ DHT22Dock.py
@@ -1,26 +1,22 @@
 import Adafruit_DHT
+import time
 
 sensor = Adafruit_DHT.DHT22
+pin = 4 # pin 7 du Raspberry PI donc GPIO4
 
-pin = '7'
-
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-"""
-from machine import Pin
-from time import sleep
-import dht
-
-capteur = dht.DHT22(Pin(7))
-
-while True:
-  try:
-    sleep(1)     # le DHT22 renvoie au maximum une mesure toute les 2s
-    capteur.measure()     # Recuperère les mesures du capteur
-    print(f"Temperature : {capteur.temperature():.1f}°C")
-    print(f"Humidite    : {capteur.humidity():.1f}%")
-  except OSError as e:
-    print("Echec reception")
-      """
+while True :
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    if humidity is not None and temperature is not None:
+        if humidity < 20 :
+            print('Température={0:0.1f}*C  Humidité={1:0.1f}%, Taux d\'humidité beaucoup trop faible !'.format(temperature, humidity))
+        elif humidity < 40 :
+            print('Température={0:0.1f}*C  Humidité={1:0.1f}%, Taux d\'humidité trop faible !'.format(temperature, humidity))
+        elif humidity < 60 :
+            print('Température={0:0.1f}*C  Humidité={1:0.1f}%, Taux d\'humidité correct !'.format(temperature, humidity))
+        elif humidity < 80 :
+            print('Température={0:0.1f}*C  Humidité={1:0.1f}%, Taux d\'humidité trop élevé !'.format(temperature, humidity))
+        else :
+            print('Température={0:0.1f}*C  Humidité={1:0.1f}%, Taux d\'humidité beaucoup trop élevé !'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')            
+    time.sleep(1)
