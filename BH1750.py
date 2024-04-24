9ed6e48e46c70f2d5e65712578bc8a8d7d2eda17
Maël LAOUITI <mael.laouiti@etu.univ-smb.fr>
2024-04-24 10:59:01 +0200

    Ajout de la transmission des donnees en MQTT. Implementation d'une fonction de test de transmission sans materiel.

--- BH1750.py
+++ BH1750.py
@@ -1,5 +1,19 @@
 import smbus
 import time
+import paho.mqtt.client as mqtt
+import json
+#import random
+from paho.mqtt.client import MQTTv311
+
+#Configuration du serveur broker MQTT
+SERVEUR = '169.254.3.30'
+#Intervale de capture et d'envoi des donnees en secondes
+INTERVALE = 5
+next_reading = time.time()
+#Configuration du packet envoye à la carte
+sensor_data = {'luminosite' : 0}
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, protocol=mqtt.MQTTv311)
 
 # Define I2C address for BH1750 sensor
 BH1750_ADDR = 0x23
@@ -10,7 +24,15 @@ CONTINUOUS_HIGH_RES_MODE = 0x10
 # Create an instance of the smbus module to communicate with I2C
 bus = smbus.SMBus(1)
 
-# Function to read light intensity from the sensor
+#Connection au Pi3 via le port MQTT par defaut and un keepalive de 60s (temps d'abandon)
+client.connect(SERVEUR, 1883, 60)
+client.loop_start()
+
+#Fonction de test de la transmission MQTT sans materiel
+#def read_light_intensity():
+#	return round(random.uniform(0,100), 2)
+
+#Function to read light intensity from the sensor
 def read_light_intensity():
     # Send command to measure light intensity
     bus.write_byte(BH1750_ADDR, CONTINUOUS_HIGH_RES_MODE)
@@ -33,9 +55,17 @@ try:
         
         # Print the light intensity
         print("Light Intensity: {} lux".format(intensity))
+        sensor_data['luminosite'] = intensity
+        #Publication des donnees sous forme de fichier json sur le topic 'lum' du broker MQTT
+        client.publish('lum',json.dumps(sensor_data),1)
         
         # Wait for some time before taking the next measurement
-        time.sleep(1)  # Adjust this value as per your requirement
+        next_reading += INTERVALE
+        sleep_time = next_reading-time.time()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
 
 except KeyboardInterrupt:
     print("Measurement stopped by the user")
+client.loop_stop()
+client.disconnect()
